79cd6157572c79d74e58956d1ccfddc3d126a89f
LuisLoquendo24 <luiseramren2@gmail.com>
2025-07-17 15:43:03 -0600

    Primer commit

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -2,6 +2,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('titulo', 'Mi Portafolio')</title>
     <link rel="stylesheet" href="{{ asset('css/estilo.css') }}">
     <style>
